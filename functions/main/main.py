ad7fd921473f5dfb3a28b7e115b475f5abf8ac9c
Flavius Stroe <stroe.flavius@gmail.com>
2021-04-13 19:45:35 +0100

    Added function

---
+++ functions/main/main.py
@@ -0,0 +1,19 @@
+from azure import functions
+import os
+
+
+def main(req: functions.HttpRequest) -> functions.HttpResponse:
+
+    with open(os.getcwd() + '/main/index.html') as index_file:
+        body = index_file.read()
+
+    headers = {
+        'Access-Control-Allow-Origin': '*'
+    }
+
+    return functions.HttpResponse(
+        body=body,
+        status_code=200,
+        headers=headers,
+        mimetype='text/html'
+    )
