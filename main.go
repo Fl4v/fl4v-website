d995a7c4446abf37e4de1c286caddf4f42f656ef
Flavius Stroe <stroe.flavius@gmail.com>
2021-08-08 21:33:00 +0100

    feat: add lambda code and index file

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"io/ioutil"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+var lambdaError string
+
+func lambdaHandler() (events.APIGatewayProxyResponse, error) {
+
+	data, err := ioutil.ReadFile("index.html")
+
+	if err != nil {
+		return events.APIGatewayProxyResponse{
+			Body:       err.Error(),
+			StatusCode: 404,
+			Headers: map[string]string{
+				"Content-Type": "text/plain",
+			},
+		}, nil
+	}
+
+	return events.APIGatewayProxyResponse{
+		Body:       string(data),
+		StatusCode: 200,
+		Headers: map[string]string{
+			"Content-Type": "text/html",
+		},
+	}, nil
+}
+
+func main() {
+	lambda.Start(lambdaHandler)
+}
