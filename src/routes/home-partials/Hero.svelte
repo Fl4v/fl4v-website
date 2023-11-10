246560e14f82b14221547942f828c58e783b4337
Flavius Stroe <stroe.flavius@gmail.com>
2023-11-10 22:39:07 +0000

    feat: add homepage components

---
+++ src/routes/home-partials/Hero.svelte
@@ -0,0 +1,27 @@
+<script>
+	import HeroComponents from './HeroComponents.svelte';
+</script>
+
+<div class="grid grid-cols-1 xl:grid-cols-2 gap-4 md:gap-10 items-center">
+	<!-- Info -->
+	<div class="flex flex-col items-center xl:items-start text-center xl:text-left space-y-4">
+		<h1 class="h1 !text-5xl md:!text-6xl max-w-[600px]">Software Engineer</h1>
+		<p class="!text-xl max-w-[475px]">
+			Portofolio website built with <a class="anchor" href="https://www.skeleton.dev/" target="_blank" rel="noreferrer"
+				>Skeleton UI</a
+			>
+			and <a class="anchor" href="https://svelte.dev/" target="_blank" rel="noreferrer">Svelte</a>.
+		</p>
+		<div class="flex gap-4">
+			<a href="https://github.com/Fl4v/fl4v.com#readme" target="_blank" class="btn variant-filled-primary">
+				<span>Documentation</span>
+				<i class="fa-solid fa-arrow-right-long" />
+			</a>
+		</div>
+	</div>
+
+	<!-- Components -->
+	<div class="hidden md:inline-block w-full max-w-[650px] mx-auto mt-20">
+		<HeroComponents />
+	</div>
+</div>
---
+++ src/routes/home-partials/HomeContent.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+	// Components
+	import { ListBox, ListBoxItem } from '@skeletonlabs/skeleton';
+	// Filter SVGs
+	import { Apollo, BlueNight, Emerald, Noir } from '@skeletonlabs/skeleton';
+
+	// Local
+	let showcase = 'components';
+</script>
+
+<Apollo />
+<BlueNight />
+<Emerald />
+<Noir />
+
+<div class="grid grid-cols-1 lg:grid-cols-4 gap-4 md:gap-10 items-start">
+	<!-- Info -->
+	<div class="col-span-2">
+		<div class="space-y-4">
+			<h2 class="h2">Welcome to my Revamped Website.</h2>
+			<p>
+				The majorty of this website has been insipred by Skeleton's UI kit. Being mainly a backend engineer I want to
+				focus on a few backend projects and showcase them here. Skeleton's UI takes away the pain of having to design a
+				website from scratch and allows me to focus on more important things.
+			</p>
+		</div>
+	</div>
+	<div class="col-span-2">
+		<div class="space-y-4">
+			<h2 class="h2">Projects</h2>
+			<ListBox active="variant-filled" hover="hover:variant-soft" rounded="rounded-container-token" padding="!p-3">
+				<ListBoxItem bind:group={showcase} name="showcase" value="components">
+					<div class="space-y-2">
+						<h3 class="h3">Fl4v.com Frontend</h3>
+						<p class="!text-sm">
+							Check the source code of this website <i class="bi bi-git" />
+							<a class="anchor" href="https://github.com/fl4v/fl4v.com" target="_blank" rel="noreferrer"
+								>fl4v/fl4v.com</a
+							>
+						</p>
+					</div>
+				</ListBoxItem>
+				<ListBoxItem bind:group={showcase} name="showcase" value="actions">
+					<div class="space-y-2">
+						<h3 class="h3">Fl4v.com Backend</h3>
+						<p class="!text-sm">Probably coming at some point in the future.</p>
+					</div>
+				</ListBoxItem>
+			</ListBox>
+		</div>
+	</div>
+</div>
